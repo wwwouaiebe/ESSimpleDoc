10c2734495b15b50fa00d6d4e895fceca919d644
wwwouaiebe <github.christian@guyette.be>
2021-10-21 17:58:35 +0200

    Initial commit

--- src/LinkBuilder.js
+++ src/LinkBuilder.js
@@ -7,9 +7,7 @@ class LinkBuilder {
 	}
 
 	getSourceLink ( classDoc ) {
-		console.log ( classDoc.rootPath );
 		let fileLink = theConfig.links.get ( classDoc.file );
-		console.log ( fileLink );
 		if ( fileLink ) {
 			return classDoc.rootPath + fileLink + '#L' + String ( classDoc.line ).padStart ( 5, '_' );
 		}
